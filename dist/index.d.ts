4cf4adad0afc36aa3360f4020f4e9f677161822c
Viktor Kirilov <deepsybg@gmail.com>
2022-05-09 23:57:06 +0300

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import './shared/main.scss';
-import { Button } from "./Molecules/Button";
-import { Checkbox } from "./Molecules/Checkbox";
-import Input from "./Molecules/Input";
-import RadioButton from "./Molecules/RadioButton";
-import { Textarea } from "./Molecules/Textarea";
-export { Button, Checkbox, Input, RadioButton, Textarea };
