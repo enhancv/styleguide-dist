13d799588bc1ebfdb0310a65ed5c8bfe1fa8be7e
Viktor Kirilov <deepsybg@gmail.com>
2022-11-03 13:00:24 +0000

    Updated styleguide

--- Molecules/IconButton/IconButton.d.ts
+++ Molecules/IconButton/IconButton.d.ts
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { HTMLAttributes, ReactNode } from "react";
 declare enum SizeType {
     sm = "sm",
     md = "md"
@@ -8,8 +8,7 @@ export declare type Props = {
     primary?: boolean;
     children?: ReactNode;
     disabled?: boolean;
-    onClick?: () => void;
     size?: `${SizeType}`;
-};
+} & HTMLAttributes<HTMLButtonElement>;
 export declare const IconButton: ({ className, primary, children, disabled, size, ...props }: Props) => JSX.Element;
 export {};
