e8e50b1159094f716092c2b111ba85ea9ca7ba7c
Viktor Kirilov <deepsybg@gmail.com>
2022-11-07 13:50:35 +0000

    Updated styleguide

--- Molecules/IconButton/IconButton.d.ts
+++ Molecules/IconButton/IconButton.d.ts
@@ -8,7 +8,8 @@ export declare type Props = {
     primary?: boolean;
     children?: ReactNode;
     disabled?: boolean;
+    title?: string;
     size?: `${SizeType}`;
 } & HTMLAttributes<HTMLButtonElement>;
-export declare const IconButton: ({ className, primary, children, disabled, size, ...props }: Props) => JSX.Element;
+export declare const IconButton: ({ className, primary, children, disabled, title, size, ...props }: Props) => JSX.Element;
 export {};
