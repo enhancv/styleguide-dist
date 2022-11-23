a02f8de2f05266f39d5a9e710dcc61a72c6673e2
Viktor Kirilov <deepsybg@gmail.com>
2022-11-23 08:53:14 +0000

    Updated styleguide

--- Molecules/MultiSlider/components/Bars.d.ts
+++ Molecules/MultiSlider/components/Bars.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="react" />
 interface Props {
     options: number;
+    setOptionLocal: (index: number) => void;
 }
-declare function Bars({ options }: Props): JSX.Element;
+declare function Bars({ options, setOptionLocal }: Props): JSX.Element;
 export default Bars;
