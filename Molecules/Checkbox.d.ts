d85d78e3ed9d196a4cfe2e4b59e80564ad517881
Viktor Kirilov <deepsybg@gmail.com>
2023-03-01 07:02:49 +0000

    Updated styleguide

--- Molecules/Checkbox.d.ts
+++ Molecules/Checkbox.d.ts
@@ -1,4 +1,5 @@
 export interface Props {
+    checked: boolean;
     id?: string;
     label?: string;
     name?: string;
@@ -6,4 +7,4 @@ export interface Props {
     defaultChecked?: boolean;
     onClick?: () => void;
 }
-export declare const Checkbox: ({ label, onClick, name, id, defaultChecked, className, }: Props) => JSX.Element;
+export declare const Checkbox: ({ checked, label, onClick, name, id, defaultChecked, className, }: Props) => JSX.Element;
