6d79cee8dacfc006c2217f4a12e57aefccde60d7
Viktor Kirilov <deepsybg@gmail.com>
2023-11-06 10:18:50 +0000

    Updated styleguide

--- Molecules/Select/SelectDropdown.d.ts
+++ Molecules/Select/SelectDropdown.d.ts
@@ -1,13 +1,17 @@
 import React, { ReactNode } from "react";
 export interface Props {
-    defaultValue?: string;
+    defaultValue?: Options;
     label?: string | JSX.Element;
-    onChange: (value: string) => void;
+    onChange: (value: Options) => void;
     className?: string;
-    options: any[];
+    options: Options[];
     fullWidth?: boolean;
     small?: boolean;
     children?: ReactNode;
+}
+export interface Options {
+    value: string | number;
+    text: string | number;
 }
 declare const SelectDropdown: ({ defaultValue, onChange, className, options, fullWidth, label, children, }: Props) => React.JSX.Element;
 export default SelectDropdown;
