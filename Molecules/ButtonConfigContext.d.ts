c687e9041f4594f62c4499851075fc09929d6a8c
Viktor Kirilov <deepsybg@gmail.com>
2024-06-11 11:40:57 +0000

    Updated styleguide

---
+++ Molecules/ButtonConfigContext.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+type ButtonConfigContextType = {
+    defaultPreserveCasing: boolean;
+};
+declare const ButtonConfigContext: import("react").Context<ButtonConfigContextType>;
+export default ButtonConfigContext;
