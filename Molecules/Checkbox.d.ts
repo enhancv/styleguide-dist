b8ac3a872b875a7721b98da06ecdc16454f79614
Viktor Kirilov <deepsybg@gmail.com>
2025-05-20 13:03:15 +0000

    Updated styleguide

--- Molecules/Checkbox.d.ts
+++ Molecules/Checkbox.d.ts
@@ -1,11 +1,20 @@
 import React from "react";
+declare enum SizeType {
+    sm = "sm",
+    md = "md",
+    lg = "lg"
+}
 export interface Props {
     checked: boolean;
     id?: string;
-    label?: string;
+    label?: string | React.ReactNode;
     name?: string;
     className?: string;
     defaultChecked?: boolean;
-    onClick?: () => void;
+    disabled?: boolean;
+    partialChecked?: boolean;
+    size?: `${SizeType}`;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
-export declare const Checkbox: ({ checked, label, onClick, name, id, defaultChecked, className, }: Props) => React.JSX.Element;
+export declare const Checkbox: React.ForwardRefExoticComponent<Props & React.RefAttributes<HTMLInputElement>>;
+export {};
